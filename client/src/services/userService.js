8a2252dc146721a43a6a3f9cd448f290e44f3533
devegalaura-dev <lauradevega94@gmail.com>
2024-10-16 19:36:57 +0200

    Change URL

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,7 +1,7 @@
 import axios from "axios"; 
 
 
-const URL = 'http://localhost:3000/musenion';  
+const URL = 'http://localhost:3000/cookinando';  
 
 //READ-metodo get
 export const getUsers = async () => {
