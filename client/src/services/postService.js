03ac61c9f4919cdb7f831bbc03db29e5d3634c48
devegalaura-dev <lauradevega94@gmail.com>
2024-10-16 19:33:24 +0200

    Add post services

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -1,1 +1,61 @@
-// Post services
+import axios from "axios"; 
+
+
+const URL = 'http://localhost:3000/musenion';  
+
+//READ-metodo get
+export const getPost = async () => {
+  try {
+    const response = await axios.get(URL); 
+    return response.data;
+  } catch (error) {
+    console.error('Error al traer los posts:', error);
+    throw error;
+  }
+};
+
+//READ-metodo get pero por ID
+export const getPostById = async (id) => {
+  try {
+    const response = await axios.get(`${URL}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error al obtener el post:', error);
+    throw error;
+  }
+};
+
+//DELETE- metodo delete
+
+export const deletePost = async (id) => {
+  try {
+      const response = await axios.delete(`${URL}/${id}`);
+      return response.data
+  } catch (error) {
+      console.error('Error al borrar el post:', error);
+      throw error;
+  }
+}
+
+//CREATE - metodo POST
+export const postNewPost = async (data) => {
+  try {
+      const response = await axios.post(URL, data);
+      return response.data
+  } catch (error) {
+      console.error('Error al crear el nuevo post:', error);
+      throw error;
+  }
+}
+
+//UPDATE . metodo put
+
+export const putPost = async (id, data) => {
+  try {
+      const response = await axios.put(`${URL}/${id}`, data);
+      return response.data
+  } catch (error) {
+      console.error('Error al actualizar el post:', error);
+      throw error;
+  }
+}
