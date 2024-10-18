1fa14c592a8969f7a9b922a967e888c376f236d5
devegalaura-dev <lauradevega94@gmail.com>
2024-10-18 13:43:36 +0200

    Add Navbar structure

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,26 @@
-export const Navbar = () => {
+export const Navbar = ({ isLoggedIn, onLogout }) => {
     return (
-      <>
-        
-      </>
+        <nav class= "mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div>
+          <img/> 
+        </div>
+        <ul className="flex space-x-6">
+          <li>
+            Sobre Nosotros
+          </li>
+          <li>
+           Recetas
+          </li>
+          <li>
+            Contacto
+          </li>
+          <li>
+            Iniciar Sesión
+          </li>
+          <li>
+           Registrarse
+          </li>
+        </ul>
+      </nav>
     );
   };
