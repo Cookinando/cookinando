f14a1f7fe712f7d944eaa0a6b70a499ded499a60
Rada749 <radames5897@gmail.com>
2024-10-23 10:37:23 +0200

    fixed bg image in layout

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { Footer } from "../components/Footer.jsx";
 import { Navbar } from "../components/Navbar.jsx";
-import backgroundImage from '../assets/images/fondo_layout.png'; 
+import backgroundImage from '../assets/images/fondoBlog.webp'; 
 
 const Layout = () => {
   return (
