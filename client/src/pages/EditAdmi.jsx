537785541e9c4f40f5b43860edace11f8dfe0cde
magalylazarte <magvita8@gmail.com>
2024-10-28 10:11:33 +0100

    seccionadmi

---
+++ client/src/pages/EditAdmi.jsx
@@ -0,0 +1,16 @@
+
+const EditAdmi = () => {
+  return (
+    <div className="bg-[url('./assets/fondo.png')] bg-cover bg-center min-h-screen text-white">
+      <section className="p-10">
+        <h1 className="text-4xl font-bold">Lista de usuarios</h1>
+        <div className="flex justify-center items-center w-full h-[500px]">
+        </div>
+        <p className="mt-2 text-center text-[#C1A881]">
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default EditAdm
