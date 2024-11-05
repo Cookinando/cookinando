0df4b57850195a0c4e66fb31e2adc948eba1ce98
devegalaura-dev <lauradevega94@gmail.com>
2024-11-05 12:21:16 +0100

    Fix Navbar; fix sintax error

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -43,7 +43,7 @@ export const Navbar = () => {
                         <li className= "pointer-events-auto w-screen flex justify-center lg:hidden"> <Link to="#" className="cursor-pointer hover:text-gray-300" onClick={() => handleNavigation("#")}> <img className= "h-5" src={closeIcon} alt="Profile icon" /></Link></li>
                         <li className= "text-light pointer-events-auto"><Link to="/"className="cursor-pointer hover:text-gray-300" onClick={() => handleNavigation("/")}>Recetas</Link></li>
                         <li className= "text-light pointer-events-auto"><Link to="/login" className="cursor-pointer hover:text-gray-300" onClick={() => handleNavigation("/login")}>Iniciar sesión</Link></li>
-                        <li className= "text-light pointer-events-auto"><Link to="/signup" className="cursor-pointer hover:text-gray-300" onClick={() => handleNavigation("/signup")}>Registrase</Link></li>
+                        <li className= "text-light pointer-events-auto"><Link to="/signup" className="cursor-pointer hover:text-gray-300" onClick={() => handleNavigation("/signup")}>Registrarse</Link></li>
                     </>
                 )}
                 {isAuthenticated && (
