613bf8ec069fc5568aca277b994bb307991475b8
devegalaura-dev <lauradevega94@gmail.com>
2024-11-06 09:59:46 +0100

    Add Navbar test; describe step by step for testing

--- client/src/test/Navbar.test.jsx
+++ client/src/test/Navbar.test.jsx
@@ -1,1 +1,14 @@
 // Navbar.test.js
+//vídeo de referencia: https://www.youtube.com/watch?v=sshsoHlNdyY
+//Falta: 
+//configurar los types
+//npm install testing-library/react testing-library/jest-dom --save-dev
+//npm install testing-library/user-event
+//crear setupTests.js dentro de la carpeta test para configurar acciones globales
+//importar afterEach
+//importar cleanup para limpiar el DOM
+//crear un archivo de test por cada componente que se quiera testear
+//importar beforeEach
+//importar render
+//importar vi
+//importar componente que se va a testear
