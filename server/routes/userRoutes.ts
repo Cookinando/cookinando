581fe46c3022edb3fcc39a7522b0ac94381719ce
Rada749 <radames5897@gmail.com>
2024-11-06 10:42:29 +0100

    fixed editprofile page

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,15 +1,13 @@
 import { Router } from "express";
-import { getUsers, getUser, editUser, deleteUser, createUser } from "../controllers/userController";
+import { getUsers, getUser, editUser, deleteUser } from "../controllers/userController";
 import { authenticateToken } from "../middlewares/authMiddleware";
-import { validateSignUp, validateUpdateProfile } from "../middlewares/validators/userValidator";
+import { validateUpdateProfile } from "../middlewares/validators/userValidator";
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
 
 userRouter.get("/:id", getUser);
-
-userRouter.post("/", validateSignUp, createUser);
 
 userRouter.put("/:id", authenticateToken, editUser);
 
