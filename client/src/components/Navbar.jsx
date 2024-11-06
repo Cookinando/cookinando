565c336674e5b2bb6e146e6bde3cecedb1833392
devegalaura-dev <lauradevega94@gmail.com>
2024-11-06 15:10:20 +0100

    Add tests for Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import  { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import profile from '../assets/images/icon_profile.svg';
 import closeIcon from '../assets/images/close_icon.svg';
--- client/src/test/Navbar.test.jsx
+++ client/src/test/Navbar.test.jsx
@@ -1,25 +1,11 @@
-// Navbar.test.js
 //vídeo de referencia: https://www.youtube.com/watch?v=sshsoHlNdyY
-//Falta: 
-//configurar los types
-//npm install testing-library/react testing-library/jest-dom --save-dev
-//npm install testing-library/user-event
-//crear setupTests.js dentro de la carpeta test para configurar acciones globales
-//importar afterEach
-//importar cleanup para limpiar el DOM
-//crear un archivo de test por cada componente que se quiera testear
-//importar beforeEach
-//importar render
-//importar vi
-//importar it
-//importar test y expect de vitest
-//importar componente que se va a testear
 
-import { expect, test } from 'vitest'
+import { expect, test, vi } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import { beforeEach, describe } from 'vitest'
 import { Navbar } from  '../components/Navbar.jsx'
-import { useContext } from '../context/AuthContext.jsx'
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
 
 vi.mock('../context/AuthContext.jsx', () => ({
     useAuth: () => ({
@@ -30,11 +16,17 @@ vi.mock('../context/AuthContext.jsx', () => ({
 
 describe('Navbar component', ()=> {
     beforeEach(()=>{
-        render(<Navbar/>)
-    })
-    test("Should show the web logo", () => {
+        render(
+        <BrowserRouter>
+        <Navbar />
+        </BrowserRouter>
+    );
+    });
+    test("Should render the web logo", () => {
         const logo = screen.getByAltText('logo')
-        expect(logo).toBeInTheDocument();
+        expect(logo).toHaveAttribute('alt', 'logo');
+    })
+    test("Should render the hamburger button on mobile",() =>{
     })
 })
 
