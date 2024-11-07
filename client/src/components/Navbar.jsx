691b393c169a6276a0dc44c1efb48fbbcfb325bb
devegalaura-dev <lauradevega94@gmail.com>
2024-11-07 10:26:07 +0100

    Fix Navbar; add option

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -43,6 +43,7 @@ export const Navbar = () => {
                 {isAuthenticated && (
                     <>
                         <li className= "pointer-events-auto w-screen flex justify-center lg:hidden"> <Link to="#" className="cursor-pointer hover:text-gray-300" onClick={() => setMenuOpen(false)}> <img className= "h-5" src={closeIcon} alt="Profile icon" /></Link></li>
+                        <li className= "text-light pointer-events-auto"><Link to="/"className="cursor-pointer hover:text-gray-300" onClick={() => setMenuOpen(false)}>Recetas</Link></li>
                         <li className= "text-light pointer-events-auto"><Link to= "/login" className="cursor-pointer hover:text-gray-300" 
                             onClick={() => {
                             logout();     
