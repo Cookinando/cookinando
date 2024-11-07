921ded4f88bc8b824ba651728e0231c6f571b4b3
LorelizDev <loreliz.dev@gmail.com>
2024-11-07 15:27:46 +0100

    chore(PrivateRoutes): delete repeted import Outlet

--- client/src/layout/PrivateRoutes.jsx
+++ client/src/layout/PrivateRoutes.jsx
@@ -1,6 +1,5 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext.jsx';
-import { Outlet } from 'react-router-dom';
 
 export const PrivateRoutes = () => {
     const { isAuthenticated } = useAuth();
