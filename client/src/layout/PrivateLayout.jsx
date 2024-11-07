d7aec3df2cf509089d2c2c51b3a6cbc6a353384d
devegalaura-dev <lauradevega94@gmail.com>
2024-11-07 09:55:41 +0100

    Fix context; add localStorage in AuthProvider for public and privates routes

--- client/src/layout/PrivateLayout.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext.jsx';
-
-export const PrivateLayout = () => {
-    const { isAuthenticated } = useAuth();
-
-    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-};
-
--- client/src/router/Router.jsx
+++ client/src/router/Router.jsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Login } from "../pages/Login.jsx";
 import { SignUp } from "../pages/SignUp.jsx";
-import PublicLayout from "../layout/PublicLayout.jsx";
+import { Layout } from "../layout/Layout.jsx";
+import { PrivateRoutes } from "../layout/PrivateRoutes.jsx";
 import Home from "../pages/Home.jsx";
 import RecipeDetail from "../pages/RecipeDetail.jsx";
 import Profile from "../pages/Profile.jsx";
@@ -9,12 +10,12 @@ import EditProfile from "../pages/EditProfile.jsx";
 import EditAdmi from "../pages/EditAdmi.jsx"
 import CreateRecipe from "../pages/CreateRecipe.jsx";
 import EditRecipe from "../pages/EditRecipe.jsx";
-import { PrivateLayout } from "../layout/PrivateLayout.jsx";
+
 
 export const router = createBrowserRouter([{
     
     path : '/',
-    element: <PublicLayout/>,
+    element: <Layout/>,
     children: [
             {
                 index: true,
@@ -30,7 +31,7 @@ export const router = createBrowserRouter([{
             },
             {
             path : 'auth',
-            element: <PrivateLayout/>,
+            element: <PrivateRoutes/>,
             children: [
                 {
                     path: 'newrecipe',
