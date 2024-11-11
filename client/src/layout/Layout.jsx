68e5310d0482c2c1adb720a3695bcc7fe7f33033
Rada749 <radames5897@gmail.com>
2024-11-11 13:00:20 +0100

    fixed authcontext and layout

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,9 +1,23 @@
 import { Outlet } from 'react-router-dom';
 import { Footer } from "../components/Footer.jsx";
 import { Navbar } from "../components/Navbar.jsx";
-import backgroundImage from '../assets/images/fondo_layout.svg'; 
+import backgroundImage from '../assets/images/fondo_layout.svg';
+import { useAuth } from '../context/AuthContext.jsx'; 
+import { useEffect } from 'react';
 
 export const Layout = () => {
+  const { checkTokenExpiration, isAuthenticated } = useAuth();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const intervalId = setInterval(() => {
+        checkTokenExpiration();
+      }, 5000);
+
+      return () => clearInterval(intervalId);
+    }
+  }, [checkTokenExpiration, isAuthenticated]);
+
   return (
     <div
       className="grid grid-rows-[auto_1fr_auto] min-h-screen bg-cover bg-center bg-fixed font-ABeeZee"
