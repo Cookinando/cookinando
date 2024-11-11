1b1fa3753cf7d315c1a909a4bf8c61b11a471d52
devegalaura-dev <lauradevega94@gmail.com>
2024-11-11 10:27:02 +0100

    Test that render Cerrar Sesión button works

--- client/src/test/Navbar.test.jsx
+++ client/src/test/Navbar.test.jsx
@@ -5,28 +5,46 @@ import { render, screen } from '@testing-library/react'
 import { beforeEach, describe } from 'vitest'
 import { Navbar } from  '../components/Navbar.jsx'
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from '../context/AuthContext.jsx'
 import '@testing-library/jest-dom';
 
-vi.mock('../context/AuthContext.jsx', () => ({
+vi.mock('../context/AuthContext.jsx', async (importOriginal) => {
+  const actual = await importOriginal();
+  return {
+    ...actual,
     useAuth: () => ({
-      isAuthenticated: true,  // o false, según lo que quieras probar
+      isAuthenticated: true,  // true o false según lo que quieras probar
       logout: vi.fn(),
     }),
-  }));
+  };
+});
+
+  // const mockUser = { 
+  //   id: 1, 
+  //   name: 'Laura', 
+  //   email: 'laura@gmail.com', 
+  //   isAuthenticated: true 
+  // };
 
 describe('Navbar component', ()=> {
     beforeEach(()=>{
         render(
-        <BrowserRouter>
-        <Navbar />
-        </BrowserRouter>
+        <AuthProvider>
+          <BrowserRouter>
+            <Navbar />
+          </BrowserRouter>
+        </AuthProvider>
     );
     });
     test("Should render the web logo", () => {
         const logo = screen.getByAltText('logo')
         expect(logo).toHaveAttribute('alt', 'logo');
-    })
-    test("Should render the hamburger button on mobile",() =>{
-    })
+    });
+
+    test('Should render "cerrar sesión" when user is authenticated', () => {
+
+      expect(screen.getByText('Cerrar Sesión')).toBeInTheDocument();
+    
+    }); 
 })
 
