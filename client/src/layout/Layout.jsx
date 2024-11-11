a2475e714261452da9aff2f40c8831dfe6fb9431
Rada749 <radames5897@gmail.com>
2024-11-11 14:29:25 +0100

    install sweetalert2 and fixed layout

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -3,24 +3,52 @@ import { Footer } from "../components/Footer.jsx";
 import { Navbar } from "../components/Navbar.jsx";
 import backgroundImage from '../assets/images/fondo_layout.svg';
 import { useAuth } from '../context/AuthContext.jsx'; 
-import { useEffect } from 'react';
+import { useEffect , useState} from 'react';
 import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 export const Layout = () => {
   const { checkTokenExpiration, isAuthenticated } = useAuth();
   const navigate = useNavigate();
 
+  const [isChecked, setIsChecked] = useState(false);
+
   useEffect(() => {
     if (isAuthenticated) {
       const intervalId = setInterval(() => {
         checkTokenExpiration();
-      }, 600000);
+      }, 600000); 
 
       return () => clearInterval(intervalId);
-    } else {
-      navigate('/login');
     }
-  }, [checkTokenExpiration, isAuthenticated, navigate]);
+
+    return;
+  }, [checkTokenExpiration, isAuthenticated]);
+
+  useEffect(() => {
+    if (!isAuthenticated && isChecked) {
+      Swal.fire({
+        title: "Su sesión ha expirado",
+        text: "Por favor inicie sesión",
+        icon: "error",
+        customClass: {
+          popup: 'bg-dark-light text-light',     
+          title: 'text-light font-bold text-lg',  
+          htmlContainer: 'text-light text-sm',   
+          confirmButton: 'bg-light-dark hover:bg-green-600 text-white font-semibold py-2 px-4 rounded'
+        },
+        buttonsStyling: false
+      }).then(() => {
+        navigate('/login');
+      });
+    }
+  }, [isAuthenticated, isChecked, navigate]);
+
+  useEffect(() => {
+    if (isAuthenticated && !isChecked) {
+      setIsChecked(true); 
+    }
+  }, [isAuthenticated, isChecked]);
 
   return (
     <div
